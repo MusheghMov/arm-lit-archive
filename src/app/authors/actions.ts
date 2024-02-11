80f1df5ae27a8accb1e8fa4d98dc7f0a8756d139
Mushegh <41339023+MusheghMov@users.noreply.github.com>
2024-02-11 21:30:33 +0400

    fix: added revalidation for server actions

--- src/app/authors/actions.ts
+++ src/app/authors/actions.ts
@@ -3,6 +3,8 @@
 import db from "@/db";
 import { authors } from "@/db/schema";
 import { z } from "zod";
+import { revalidateTag } from "next/cache";
+
 const authorScheme = z.object({
   name: z.string().min(1, {
     message: "Author name is required",
@@ -19,11 +21,13 @@ export async function createAuthor(data: z.infer<typeof authorScheme>) {
     birthDate: data.birthDate,
     deathDate: data.deathDate,
   });
+  revalidateTag("authors");
 
   return res;
 }
 
 export async function getAuthors() {
   const res = await db.select().from(authors);
+  revalidateTag("authors");
   return res;
 }
