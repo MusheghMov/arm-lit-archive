3cc184fb7521991a16614b31bdb10151236d096a
Mushegh <41339023+MusheghMov@users.noreply.github.com>
2024-02-19 06:21:34 +0400

    added book adding card

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,12 +1,16 @@
+import { AddBookCard } from "./AddBookCard";
+import { getAuthors } from "./actions";
 import BooksContainer from "./BooksContainer";
 import Books from "./Books";
 
 export default async function AuthorsPage() {
+  const authors = await getAuthors();
   return (
     <div className="flex w-full flex-row items-start justify-between space-x-10 overflow-scroll px-8 py-4">
       <BooksContainer>
         <Books />
       </BooksContainer>
+      <AddBookCard authors={authors} />
     </div>
   );
 }
