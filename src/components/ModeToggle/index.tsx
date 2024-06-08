02eda5f210318292976f35b58b9eb498654f4c51
Mushegh <41339023+MusheghMov@users.noreply.github.com>
2024-06-09 01:37:48 +0400

    fix: made modeToggle component popover

--- src/components/ModeToggle/index.tsx
+++ src/components/ModeToggle/index.tsx
@@ -4,106 +4,94 @@ import { Circle, Minus, Plus } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useAtom } from "jotai";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { fontSize as storeFontSize } from "@/providers/JotaiProvider";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
-  const [fontSize, setFontSize] = useAtom(storeFontSize)
+  const [fontSize, setFontSize] = useAtom(storeFontSize);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <Popover>
+      <PopoverTrigger asChild>
         <Button variant="outline" size="icon" className="rounded-full">
           <Circle className="fill-main stroke-border" />
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        align="end"
-        className="flex flex-col gap-3 p-3 items-center bg-card"
-      >
+      </PopoverTrigger>
+      <PopoverContent className="flex flex-col items-center gap-3 bg-card p-3">
         <div className="flex flex-row items-center gap-1 bg-card">
-          <DropdownMenuItem asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="cursor-pointer rounded-full"
-              onClick={() => setTheme("light")}
-            >
-              <Circle className="fill-white stroke-gray-300" />
-            </Button>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="cursor-pointer rounded-full"
-              onClick={() => setTheme("slate")}
-            >
-              <Circle className="fill-slate-500 stroke-slate-500" />
-            </Button>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild className="p-0">
-            <Button
-              variant="outline"
-              size="icon"
-              className="cursor-pointer rounded-full"
-              onClick={() => setTheme("green")}
-            >
-              <Circle className="fill-green-500 stroke-green-500" />
-            </Button>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="cursor-pointer rounded-full"
-              onClick={() => setTheme("zink")}
-            >
-              <Circle className="fill-zinc-500 stroke-zinc-500" />
-            </Button>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="cursor-pointer rounded-full"
-              onClick={() => setTheme("violet")}
-            >
-              <Circle className="fill-violet-500 stroke-violet-500" />
-            </Button>
-          </DropdownMenuItem>
-          <DropdownMenuItem asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="cursor-pointer rounded-full"
-              onClick={() => setTheme("blue")}
-            >
-              <Circle className="fill-blue-500 stroke-blue-500" />
-            </Button>
-          </DropdownMenuItem>
+          <Button
+            variant="outline"
+            size="icon"
+            className="cursor-pointer rounded-full"
+            onClick={() => setTheme("light")}
+          >
+            <Circle className="fill-white stroke-gray-300" />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="cursor-pointer rounded-full"
+            onClick={() => setTheme("slate")}
+          >
+            <Circle className="fill-slate-500 stroke-slate-500" />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="cursor-pointer rounded-full"
+            onClick={() => setTheme("green")}
+          >
+            <Circle className="fill-green-500 stroke-green-500" />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="cursor-pointer rounded-full"
+            onClick={() => setTheme("zink")}
+          >
+            <Circle className="fill-zinc-500 stroke-zinc-500" />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="cursor-pointer rounded-full"
+            onClick={() => setTheme("violet")}
+          >
+            <Circle className="fill-violet-500 stroke-violet-500" />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            className="cursor-pointer rounded-full"
+            onClick={() => setTheme("blue")}
+          >
+            <Circle className="fill-blue-500 stroke-blue-500" />
+          </Button>
         </div>
-        <div className="flex gap-2 justify-between px-1 flex-row items-center w-full">
-          <p className="w-full uppercase font-semibold flex-1">Font Size:</p>
+        <div className="flex w-full flex-row items-center justify-between gap-2 px-1">
+          <p className="w-full flex-1 font-semibold uppercase">Font Size:</p>
           <div className="flex flex-row items-center gap-2">
-            <Button className="h-min w-min p-2 rounded-full" onClick={() => {
-              setFontSize(fontSize - 1)
-            }}><Minus size={16} /></Button>
-            <p>{fontSize}</p>
-            <Button className="h-min w-min p-2 rounded-full"
+            <Button
+              className="h-min w-min rounded-full p-2"
               onClick={() => {
-                setFontSize(fontSize + 1)
+                setFontSize(fontSize - 1);
               }}
-            ><Plus size={16} /></Button>
+            >
+              <Minus size={16} />
+            </Button>
+            <p>{fontSize}</p>
+            <Button
+              className="h-min w-min rounded-full p-2"
+              onClick={() => {
+                setFontSize(fontSize + 1);
+              }}
+            >
+              <Plus size={16} />
+            </Button>
           </div>
         </div>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </PopoverContent>
+    </Popover>
   );
 }
